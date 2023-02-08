be059fab89fc9ef6f3f59abffb2de4ebe467ab43
Loi Van Tran <datloiboi@gmail.com>
2023-02-08 15:41:31 -0500

    Change transpose to 1 liner

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    res = []
     matrix = request.get_json()
 
     # O(M*N) where m is the length of the shortest list and n is the number of lists
-    for col in zip(*matrix):
-        res.append(col)
+    res = [col for col in zip(*matrix)]
     return jsonify(res)
 
 
